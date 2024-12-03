0d668625c64d9ecf4421d304f5364bbbd130cc98
angryluck <54353246+angryluck@users.noreply.github.com>
2024-12-03 08:46:24 +0100

    Added template

---
+++ template.py
@@ -0,0 +1,28 @@
+from aocd import get_data
+
+# Data
+data = get_data(year=2024, day=0)
+
+test = """
+"""
+
+# Convert data (text) to workable input
+def parse(text:str):
+    return
+
+# Part 1
+
+def part1(text:str) -> int:
+    return
+
+print(part1(test))
+print(part1(data))
+
+
+# Part 2
+
+def part2(text:str) -> int:
+    return
+
+print(part2(test))
+print(part2(data))
